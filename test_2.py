0f355057c0228b4ae108aca5a921d85d9e4cbe02
dlwlrmaIU0516 <53083389+dlwlrmaIU0516@users.noreply.github.com>
2025-08-27 22:32:13 +0900

    Create test_2.py

---
+++ test_2.py
@@ -0,0 +1,77 @@
+# Import or install Sionna
+try:
+    import sionna.rt
+except ImportError as e:
+    import os
+    os.system("pip install sionna-rt")
+    import sionna.rt
+
+# Other imports
+%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+
+no_preview = True # Toggle to False to use the preview widget
+
+# Import relevant components from Sionna RT
+from sionna.rt import load_scene, PlanarArray, Transmitter, Receiver, Camera,\
+                      PathSolver, RadioMapSolver, subcarrier_frequencies
+
+scene = load_scene(sionna.rt.scene.munich, merge_shapes=True) # Merge shapes to speed-up computations
+
+# Configure antenna array for all transmitters
+scene.tx_array = PlanarArray(num_rows=1,
+                             num_cols=1,
+                             vertical_spacing=0.5,
+                             horizontal_spacing=0.5,
+                             pattern="tr38901",
+                             polarization="V")
+
+# Configure antenna array for all receivers
+scene.rx_array = PlanarArray(num_rows=1,
+                             num_cols=1,
+                             vertical_spacing=0.5,
+                             horizontal_spacing=0.5,
+                             pattern="dipole",
+                             polarization="cross")
+
+# Create transmitter
+tx = Transmitter(name="tx",
+                 position=[8.5,21,27],
+                 display_radius=2)
+
+# Add transmitter instance to scene
+scene.add(tx)
+
+# Create a receiver
+rx = Receiver(name="rx",
+              position=[45,90,1.5],
+              display_radius=2)
+
+# Add receiver instance to scene
+scene.add(rx)
+
+tx.look_at(rx) # Transmitter points towards receiver
+
+# Instantiate a path solver
+# The same path solver can be used with multiple scenes
+p_solver  = PathSolver()
+
+# Compute propagation paths
+paths = p_solver(scene=scene,
+                 max_depth=5,
+                 los=True,
+                 specular_reflection=True,
+                 diffuse_reflection=False,
+                 refraction=True,
+                 synthetic_array=False,
+                 seed=41)
+
+
+a, tau = paths.cir(normalize_delays=True, out_type="numpy")
+
+# Shape: [num_rx, num_rx_ant, num_tx, num_tx_ant, num_paths, num_time_steps]
+print("Shape of a: ", a.shape)
+
+# Shape: [num_rx, num_rx_ant, num_tx, num_tx_ant, num_paths]
+print("Shape of tau: ", tau.shape)
