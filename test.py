57760cca6837b04f21f05bd76d35b16dcc257deb
dlwlrmaIU0516 <53083389+dlwlrmaIU0516@users.noreply.github.com>
2025-08-27 23:51:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -92,7 +92,7 @@ rm = rm_solver(
 
 
 
-pos = np.array([[-59.9067, -67.074, 0]])
+pos = np.array([[45,90,1.5]])
 # Remove any existing receivers if present
 for key in list(scene.receivers.keys()):
     scene.remove(str(key))
@@ -129,3 +129,4 @@ for idx in range(10):
     print(f" 코드 런타임: {runtime:.3f} 초")
     time_data.append(runtime)
 print(f" 코드 평균 런타임: {runtime/10:.3f} 초")
+
